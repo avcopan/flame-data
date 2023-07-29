c47fe8c5731cf01e295cbb52bceffc36b51ecab1
Andreas Copan <avcopan@gmail.com>
2023-07-29 17:40:19 -0400

    Fills in API documentation

--- api/api.py
+++ api/api.py
@@ -7,9 +7,12 @@ app = api_utils.start_app()
 bcrypt = flask_bcrypt.Bcrypt(app)
 
 
-@app.route("/api/@me")
+@app.route("/api/@me", methods=["GET"])
 def get_current_user():
-    # Get the current session for this user based on cookie
+    """@api {get} /api/@me Get user identity for this session, based on cookies
+
+    @apiSuccess {Object} The user's information; keys `id`, `email`, `admin`
+    """
     id = flask.session.get("user_id", None)
     if id is None:
         return api_utils.response(401, error="Unauthorized")
@@ -20,6 +23,12 @@ def get_current_user():
 
 @app.route("/api/login", methods=["POST"])
 def login_user():
+    """@api {post} /api/login Login and start a new session with cookies
+
+    @apiBody {String} email Email for authentication
+    @apiBody {String} password Password for authentication
+    @apiSuccess {Object} The user's information; keys `id`, `email`, `admin`
+    """
     email = flask.request.json.get("email")
     password = flask.request.json.get("password")
 
@@ -39,13 +48,21 @@ def login_user():
 
 @app.route("/api/logout", methods=["POST"])
 def logout_user():
-    # Remove the current session for this user based on cookie
+    """@api {post} /api/logout Logout and end this session, clearing cookies
+    """
     flask.session.pop("user_id")
     return api_utils.response(200)
 
 
 @app.route("/api/register", methods=["POST"])
 def register_user():
+    """@api {post} /api/register Register as a new user and start a new session
+    with cookies
+
+    @apiBody {String} email Email for authentication
+    @apiBody {String} password Password for authentication
+    @apiSuccess {Object} The user's information; keys `id`, `email`, `admin`
+    """
     email = flask.request.json.get("email")
     password = flask.request.json.get("password")
 
