900e8d4736684cd9e1ea4460cf2728c9f0ae8e12
Andreas Copan <avcopan@gmail.com>
2023-08-09 20:24:08 -0400

    Adds delete button in modal window on front-end

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,13 +1,15 @@
 import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import actions from "../state/actions";
 import SpeciesDetailItem from "../components/SpeciesDetailItem";
 import FormattedFormula from "../components/FormattedFormula";
+import { ExclamationCircleIcon } from "@heroicons/react/24/outline";
 
 export default function DetailPage() {
   const { connId } = useParams();
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const speciesDetails = useSelector((store) => store.speciesDetails);
   const isomerList = speciesDetails[connId];
 
@@ -15,11 +17,14 @@ export default function DetailPage() {
     dispatch(actions.getSpeciesDetails(connId));
   }, []);
 
-  console.log(isomerList);
+  const deleteThisSpecies = () => {
+    dispatch(actions.deleteSpecies(connId));
+    navigate("/");
+  };
 
   return (
     isomerList && (
-      <div>
+      <div className="max-w-screen-xl">
         <div className="stats shadow">
           <div className="stat">
             <div className="stat-title">Formula</div>
@@ -29,20 +34,40 @@ export default function DetailPage() {
           </div>
           <div className="stat">
             <div className="stat-title">SMILES</div>
-            <div className="stat-value">
-              {isomerList[0].conn_smiles}
-            </div>
+            <div className="stat-value">{isomerList[0].conn_smiles}</div>
           </div>
           <div className="stat">
             <div className="stat-title">Multiplicity</div>
-            <div className="stat-value">
-              {isomerList[0].spin_mult}
-            </div>
+            <div className="stat-value">{isomerList[0].spin_mult}</div>
           </div>
         </div>
-        {isomerList.map((isomer) => (
-          <SpeciesDetailItem key={isomer.id} isomer={isomer} />
-        ))}
+        <div className="mb-8 flex flex-col">
+          {isomerList.map((isomer) => (
+            <SpeciesDetailItem key={isomer.id} isomer={isomer} />
+          ))}
+        </div>
+        {/* Open the modal using ID.showModal() method */}
+        <button
+          className="btn btn-error btn-outline"
+          onClick={() => window.deletion_dialog.showModal()}
+        >
+          Delete
+        </button>
+        <dialog id="deletion_dialog" className="modal">
+          <form method="dialog" className="modal-box">
+            <ExclamationCircleIcon className="mb-4 h-8 w-8 text-error" />
+            <h3 className="text-lg">
+              Are you sure? This will delete all records for species {connId}.
+            </h3>
+            <div className="modal-action">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn">No</button>
+              <button className="btn btn-error" onClick={deleteThisSpecies}>
+                Yes
+              </button>
+            </div>
+          </form>
+        </dialog>
       </div>
     )
   );
