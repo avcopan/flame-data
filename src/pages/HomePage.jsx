fb710009ba6327e146c4b5a63a225baacb8d862f
Andreas Copan <avcopan@gmail.com>
2023-08-15 15:38:19 -0400

    Adds collections container to front-end

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import { useDispatch } from "react-redux";
 import actions from "../state/actions";
 import BinarySelector from "../components/BinarySelector";
 import SpeciesList from "../components/SpeciesList";
+import SideCart from "../components/SideCart";
 
 export default function HomePage() {
   const dispatch = useDispatch();
@@ -20,29 +21,42 @@ export default function HomePage() {
   };
 
   return (
-    <div>
-      <div className="flex flex-row mb-8">
-        <input
-          type="text"
-          spellCheck={false}
-          placeholder="Search by formula..."
-          className="input input-bordered w-full max-w-xl mr-2"
-          value={searchFormula}
-          onChange={(e) => setSearchFormula(e.target.value)}
-        />
-        <button className="btn btn-outline" onClick={submitSearch}>
-          Search
-        </button>
+    <div className="flex flex-row gap-6 justify-between">
+      <div className="w-fit flex flex-col">
+        <div className="flex flex-row mb-8">
+          <input
+            type="text"
+            spellCheck={false}
+            placeholder="Search by formula..."
+            className="input input-bordered w-full max-w-xl mr-2"
+            value={searchFormula}
+            onChange={(e) => setSearchFormula(e.target.value)}
+          />
+          <button className="btn btn-outline" onClick={submitSearch}>
+            Search
+          </button>
+        </div>
+        <div className="flex flex-row items-start mb-8">
+          <BinarySelector
+            topText="Partial match"
+            bottomText="Exact match"
+            topSelected={searchPartial}
+            setTopSelected={setSearchPartial}
+          />
+        </div>
+        <SpeciesList />
       </div>
-      <div className="flex flex-row items-start mb-8">
-        <BinarySelector
-          topText="Partial match"
-          bottomText="Exact match"
-          topSelected={searchPartial}
-          setTopSelected={setSearchPartial}
-        />
+      <div className="join join-vertical w-96">
+        <div className="collapse join-item border border-primary">
+          <input type="radio" name="my-accordion-2" />
+          <div className="collapse-title text-xl text-primary font-medium">
+            My Data
+          </div>
+          <div className="collapse-content">
+            <p>hello</p>
+          </div>
+        </div>
       </div>
-      <SpeciesList />
     </div>
   );
 }
