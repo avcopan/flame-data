1849563a5daae61b0dfe0872e91fcc473c61caea
Andreas Copan <avcopan@gmail.com>
2023-08-16 15:37:25 -0400

    Completes front-end add-to-collection set-up

--- src/components/SpeciesList.jsx
+++ src/components/SpeciesList.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import SpeciesItem from "./SpeciesItem";
 
 export default function SpeciesList({ speciesList, className = "" }) {
   const [selected, setSelected] = useState([]);
+  const collections = useSelector((store) => store.collections);
 
   let speciesListWithGroupIndicator = [];
   let last_formula = "";
@@ -30,6 +32,14 @@ export default function SpeciesList({ speciesList, className = "" }) {
     };
   };
 
+  const addSelectedToCollection = (collection) => {
+    return () => {
+      console.log("Adding to collection:", collection.name);
+      console.log("Collection ID:", collection.id);
+      setSelected([]);
+    };
+  };
+
   return (
     <div
       className={`flex flex-wrap gap-8 justify-start items-end ${className}`}
@@ -44,6 +54,31 @@ export default function SpeciesList({ speciesList, className = "" }) {
           checkHandler={checkHandler(species.conn_id)}
         />
       ))}
+      {selected.length &&
+        (collections.length > 1 ? (
+          <div className="dropdown dropdown-top dropdown-hover fixed bottom-4 left-4">
+            <label tabIndex={0} className="btn btn-primary m-1">
+              Add to Collection
+            </label>
+            <ul
+              tabIndex={0}
+              className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              {collections.map((collection, index) => (
+                <li onClick={addSelectedToCollection(collection)} key={index}>
+                  <a>{collection.name}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ) : (
+          <button
+            onClick={addSelectedToCollection(collections[0])}
+            className="btn btn-primary m-1 fixed bottom-4 left-4"
+          >
+            Add to Collection
+          </button>
+        ))}
     </div>
   );
 }
